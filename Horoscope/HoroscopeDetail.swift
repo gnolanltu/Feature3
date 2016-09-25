25b9d795f0b50b3bf264ae81e94e4b478d011f1a
Godfrey Nolan <godfrey@riis.com>
2016-09-25 11:00:09 -0400

    no class

---
+++ Horoscope/HoroscopeDetail.swift
@@ -0,0 +1,13 @@
+//
+//  HoroscopeDetail.swift
+//  Horoscope
+//
+//  Created by User on 9/24/16.
+//  Copyright © 2016 RIIS. All rights reserved.
+//
+
+import UIKit
+
+class HoroscopeDetail: NSObject {
+    var horoscopeName:String?
+}
